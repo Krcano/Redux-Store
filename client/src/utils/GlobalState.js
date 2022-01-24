15b1717d01ec558c5136bed2edf8827defda14b8
Kelly R Cano <krcano2001@gmail.com>
2022-01-24 15:02:20 -0800

    added store.js and Redux store to each component

---
+++ client/src/utils/GlobalState.js
@@ -0,0 +1,25 @@
+import React from "react"; // { createContext, useContext }
+// import { useProductReducer } from './reducers'
+
+import store from "./store";
+import { Provider } from "react-redux";
+// const StoreContext = createContext();
+// const { Provider } = StoreContext;
+
+export default function StoreProvider(props) {
+  // const [state, dispatch] = useProductReducer({
+  //   products: [],
+  //   cart: [],
+  //   cartOpen: false,
+  //   categories: [],
+  //   currentCategory: '',
+  // });
+
+  return <Provider store={store} {...props} />;
+}
+
+// const useStoreContext = () => {
+//   return useContext(StoreContext);
+// };
+
+// export { StoreProvider, useStoreContext };
